2ab5c967eda6386b307ece0b9f8589d8b606abbe
piguicorn <hello.piguicorn@gmail.com>
2022-06-09 12:59:01 +0200

    finished :)

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,16 @@
 import React from "react"
 
 export default function AboutMe() {
-    return (<p>About me here</p>)
+    return (
+        <section className="about-me">
+            <section className="about-me__section">
+                <h2>About</h2>
+                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            </section>
+            <section className="about-me__section">
+                <h2>Interests</h2>
+                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </section>
+        </section>
+    )
 }
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,5 +1,34 @@
 import React from "react"
+import {TwitterIcon, 
+        FacebookIcon,
+        InstagramIcon,
+        GithubIcon} from "./Icons"
 
 export default function Social() {
-    return (<p>Social here</p>)
+    return (
+        <section className="social">
+            <ul>
+                <li>
+                    <a href="#">
+                        <TwitterIcon title="Twitter"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <FacebookIcon title="Facebook"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <InstagramIcon title="Instagram"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <GithubIcon title="Github"/>
+                    </a>
+                </li>                                
+            </ul>
+        </section>
+    )
 }
